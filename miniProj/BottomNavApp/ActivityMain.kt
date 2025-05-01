72fe6a65f2ca44a1201f133f21d997385f358918
Ankit Kumar <ankit485803@gmail.com>
2025-05-01 21:30:21 +0530

    Bottom Navigation app we are trying to create

---
+++ miniProj/BottomNavApp/ActivityMain.kt
@@ -0,0 +1,51 @@
+
+
+package com.ayush.popupmenu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.PopupMenu
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.lang.Exception
+
+
+// link ye hai Ankit https://www.geeksforgeeks.org/bottom-navigation-bar-in-android-using-kotlin/
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomNav : BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        loadFragment(HomeFragment())
+        bottomNav = findViewById(R.id.bottomNav) as BottomNavigationView
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.message -> {
+                    loadFragment(ChatFragment())
+                    true
+                }
+                R.id.settings -> {
+                    loadFragment(SettingFragment())
+                    true
+                }
+            }
+        }
+    }
+    private fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
+    }
+
+}
---
+++ miniProj/BottomNavApp/about_work.kt
@@ -0,0 +1,27 @@
+
+
+/*
+
+1st April 2025 (Thursday) create the bottom navigation app, 
+
+a new blank proejct and implement bottom navigation.
+src:  https://www.geeksforgeeks.org/bottom-navigation-bar-in-android-using-kotlin/
+
+
+to download the freee  Open-licensed SVG Vector and Icons   https://www.svgrepo.com/
+
+
+work2: FAB naviation
+
+
+Nighttime light remote sensing for urban applications: Progress, challenges, and prospects
+NASA Rohita Neelam work - on Nighttime light Remote sension
+https://www.sciencedirect.com/science/article/pii/S0924271623001521
+
+https://www.earthdata.nasa.gov/topics/human-dimensions/nighttime-lights
+
+
+
+
+for crating https://www.geeksforgeeks.org/how-to-create-a-new-fragment-in-android-studio/
+*/
